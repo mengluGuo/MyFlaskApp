cf0476e481e8a7d2fc54c77677f3c66a7f28c4ff
MengluGuo <gml1988109@gmail.com>
2018-05-20 21:45:02 +0100

    Initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.',
+            'author':'Yanchao',
+            'create_date':'02-04-2018'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.',
+            'author':'Luyi',
+            'create_date':'02-04-2018'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.This is the article body.',
+            'author':'Jone Grey',
+            'create_date':'02-04-2018'
+        }
+    ]
+    return articles
